56e63cdb92f1facb46531b6c460e15f250d8e18e
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-09-17 11:56:43 +0200

    Very first skeleton

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+setup(name='pm',
+      version=version,
+      description="Project and Data management for NGI Sweden",
+      long_description='This package contains a set of functionalities that are '
+                       'useful in the day-to-day tasks of bioinformatitians in '
+                       'National Genomics Infrastructure in Stockholm, Sweden.',
+      keywords='bioinformatics',
+      author='Guillermo Carrasco',
+      author_email='guillermo.carrasco@scilifelab.se',
+      url='https://portal.scilifelab.se/genomics/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ])
