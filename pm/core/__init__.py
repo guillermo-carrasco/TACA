983af115b6ef433cb37535ee31e6bdcc689a47f5
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-09-17 15:39:19 +0200

    Moved out BaseController and added StorageController

--- pm/core/__init__.py
+++ pm/core/__init__.py
@@ -2,13 +2,25 @@ from cement.core import controller
 
 hello_message = "Welcome to Project Management tools!"
 
-# define an application base controller
 class BaseController(controller.CementBaseController):
+    """ Define an application BaseController
+
+    The most basic controller. To be used as a template for new and more complex
+    controllers.
+    """
     class Meta:
         label = 'base'
         description = hello_message
 
-    # This is for testing purposes. As it is hidden, won't show in --help
+
+    @controller.expose(hide=True)
+    def default(self):
+        print "Execute pm --help to display available commands"
+
     @controller.expose(hide=True, help="Prints a hello message")
     def hello(self):
+        """ Testing method that just prints a hello message.
+
+        Will not be listed as an available option (--hide)
+        """
         self.app.log.info(hello_message)
