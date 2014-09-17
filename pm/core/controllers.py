2c5fdf4c876b150946b261fb35b4b578033f0e06
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-09-17 16:32:12 +0200

    Separate controllers!

---
+++ pm/core/controllers.py
@@ -0,0 +1,43 @@
+from cement.core import controller
+
+hello_message = "Welcome to Project Management tools!"
+
+class BaseController(controller.CementBaseController):
+    """ Define an application BaseController
+
+    The most basic controller. To be used as a template for new and more complex
+    controllers.
+    """
+    class Meta:
+        label = 'base'
+        description = hello_message
+
+
+    @controller.expose(hide=True)
+    def default(self):
+        print "Execute pm --help to display available commands"
+
+    @controller.expose(hide=True, help="Prints a hello message")
+    def hello(self):
+        """ Testing method that just prints a hello message.
+
+        Will not be listed as an available option (--hide)
+        """
+        self.app.log.info(hello_message)
+
+
+class StorageController(BaseController):
+    """ Storage Controller
+
+    Entry point for all functionalities related to storage
+    """
+    class Meta:
+        label = 'storage'
+        description = "Entry point for all functionalities related to storage"
+        stacked_on = 'base'
+        stacked_type = 'nested'
+
+
+    @controller.expose(help="Cleans old runs from the filesystem")
+    def cleanup(self):
+        raise NotImplementedError('To be implemented...')
--- pm/core/storage.py
+++ pm/core/storage.py
@@ -1,21 +1,2 @@
-""" Controllers and methods related with storage
+""" Methods related with storage
 """
-from cement.core import controller
-
-from pm.core import BaseController
-
-class StorageController(BaseController):
-    """ Storage Controller
-
-    Entry point for all functionalities related to storage
-    """
-    class Meta:
-        label = 'storage'
-        description = "Entry point for all functionalities related to storage"
-        stacked_on = 'base'
-        stacked_type = 'nested'
-
-
-    @controller.expose(help="Cleans old runs from the filesystem")
-    def cleanup(self):
-        raise NotImplementedError('To be implemented...')
