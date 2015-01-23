f8c394f2294c17590561b6d4d86956bff2ad5e11
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-01-23 10:01:13 +0100

    Fixed some small bugs. TODO: Check adler32

--- pm/controllers/storage.py
+++ pm/controllers/storage.py
@@ -68,24 +68,30 @@ class StorageController(BaseController):
 
         :param str run: Run directory
         """
-        def _send_to_swestore(f, dest):
-            self.app.log.info('oh yeah!')
+        def _send_to_swestore(f, dest, remove=True):
+            """ Send file to swestore checking adler32 on destination and eventually
+            removing the file from disk
+
+            :param str f: File to remove
+            :param str dest: Destination directory in Swestore
+            :param bool remove: If True, remove original file from source
+            """
+            self.app.log.info("Sending {} to swestore".format(f))
             misc.call_external_command('iput -K -P {file} {dest}'.format(file=f, dest=dest),
                     with_log_files=True)
+            # XXX Check adler32
+            self.app.log.info('Run {} sent correctly and double-check was okay.'.format(f))
+            if remove:
+                self.app.log.info('Removing run'.format(f))
+                shutil.rmtree(run)
+
 
         if run.endswith('bz2'):
-            self.app.log.info("Sending tarball {} to swestore".format(run))
-            _send_to_swestore(run, self.app.config.get('storage', 'irods', 'irodsHome'))
-            # XXX Check adler32 after being sent
-            self.app.log('Run {} send correctly and double-check was okay. Removing run'.format(run))
-            shutil.rmtree(run)
+            _send_to_swestore(run, self.app.config.get('storage', 'irods').get('irodsHome'))
         else:
             self.app.log.info("Compressing run {}".format(run))
-            #import ipdb; ipdb.set_trace()
             # Compress with pbzip2
             misc.call_external_command('tar --use-compress-program=pbzip2 -cf {run}.tar.bz2 {run}'.format(run=run))
             self.app.log.info('Run {} successfully compressed! Removing from disk...'.format(run))
             shutil.rmtree(run)
-            # Send to swestore
-            self.app.log.info("Sending tarball {}.tar.bz2 to swestore".format(run))
-            _send_to_swestore('{}.tar.bz2'.format(run), self.app.config.get('storage', 'irods', 'irodsHome'))
+            _send_to_swestore('{}.tar.bz2'.format(run), self.app.config.get('storage', 'irods').get('irodsHome'))
