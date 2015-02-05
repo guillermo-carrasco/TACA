b3631f76f43cfb042fbd803875567bbe4702c92e
Francesco Vezzi <francesco.vezzi@preproc1.scilifelab.se>
2015-02-05 14:53:09 +0100

    not trying to transfer demultiplexed flowcell in function run_bcl2fastq

--- scripts/run_tracker.py
+++ scripts/run_tracker.py
@@ -413,10 +413,10 @@ def run_bcl2fastq(run, config):
         #misc.call_external_command(cl, with_log_files=True)
         misc.call_external_command_detached(cl, with_log_files=True)
 
-        LOG.info(("BCL to FASTQ conversion and demultiplexing finished for "
+        #LOG.info(("BCL to FASTQ conversion and demultiplexing finished for "
                   "run {} on {}".format(os.path.basename(run), datetime.now())))
         # Transfer the processed data to the analysis server
-        transfer_run(run, config)
+        #transfer_run(run, config)
 
 
 if __name__=="__main__":
