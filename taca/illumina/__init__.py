565d98296da637e364bae7a231523b9ea4a8c4d2
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-03-24 16:24:06 +0100

    Run type extracted. Assigned correct demultiplexing method

--- taca/illumina/__init__.py
+++ taca/illumina/__init__.py
@@ -4,19 +4,19 @@ import os
 
 from xml.etree import ElementTree as ET
 
-def demultiplex_Hiseq_X():
+def demultiplex_HiSeq_X():
     """ Demultiplexing for HiSeq X runs
     """
     pass
 
 
-def demultiplex_Hiseq():
+def demultiplex_HiSeq():
     """ Demultiplexing for HiSeq (V3/V4) runs
     """
     pass
 
 
-def demultiplex_Miseq():
+def demultiplex_MiSeq():
     """ Demultiplexing for MiSeq runs
     """
     pass
@@ -31,25 +31,44 @@ class Run(object):
             raise RuntimeError('Could not locate run directory {}'.format(run_dir))
         self.run_dir = run_dir
         self.id = os.path.basename(run_dir)
-        run_info = self._extract_run_info()
-        # XXX: Determine run type from runParameters.xml or wherever
-        # XXX: Fill in common attributes (run id, maybe number of files, etc)
-        # XXX: Instanciate correct run type
+        self._extract_run_info()
+
+        if self.run_type == 'HiSeq':
+            self.demultiplex = demultiplex_HiSeq()
+        elif self.run_type == 'HiSeq X':
+            self.demultiplex = demultiplex_HiSeq_X()
+        elif self.demultiplex == 'MiSeq':
+            self.demultiplex = demultiplex_MiSeq()
 
 
     @property
     def is_finished(self):
         """ Returns true if the run is finished, false otherwise
         """
-        # XXX: Check RTAComplete.txt existence
+        return os.path.exists(os.path.join(self.run_dir, 'RTAComplete.txt'))
 
 
     def _extract_run_info(self):
-        """ Extract run info from runParameters.xml
+        """ Extracts run info from runParameters.xml and adds it to the class attributes
 
-        :returns dict info: Information about the run
+        TODO: This method could be used to extract A LOT of information about the
+        run and maybe... populate statusdb or similar? Just leaving this comment here
+        till now...
         """
-        run_info = {}
-        run_parameters = ET.parse(os.path.join(self.run_dir, 'runParameters.xml'))
-        
-        return run_info
+
+        run_parameters = ET.parse(os.path.join(self.run_dir, 'runParameters.xml')).getroot().find('Setup')
+        # HiSeq and HiSeq X runParameter.xml files will have a Flowcell child with run type info
+        run_type = run_parameters.find('Flowcell')
+        # But MiSeqs doesn't...
+        if not run_type:
+            run_type = run_parameters.find('ApplicationName')
+
+        try:
+            if 'HiSeq X' in run_type.text:
+                self.run_type = 'HiSeq X'
+            elif 'HiSeq Flow Cell' in run_type.text:
+                self.run_type = 'HiSeq'
+            elif 'MiSeq' in run_type.text:
+                self.run_type = 'MiSeq'
+        except AttributeError:
+            raise RuntimeError('Run type could not be determined for run {}'.format(self.id))
