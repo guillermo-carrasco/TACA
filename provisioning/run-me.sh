fa6dfdc5213582aba8795dee2bffe46a8b00f6dc
Robin Andeer <robin.andeer@gmail.com>
2015-04-20 11:11:43 +0200

    install package before dev dependencies (dependency links) add statusdb back to requirements.txt

--- provisioning/run-me.sh
+++ provisioning/run-me.sh
@@ -1,9 +1,10 @@
 # activate environment
 source activate develop
 
-# install development dependencies
 cd /vagrant
-pip install -r ./requirements-dev.txt
 
 # install the package for development
 python setup.py develop
+
+# install development dependencies
+pip install -r ./requirements-dev.txt
